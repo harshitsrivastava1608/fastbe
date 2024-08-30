410ca8ed58ad7c4f17c997dcf8ad23e20f020aab
harshitsrivastava1608 <harshit.srivastava1608@gmail.com>
2024-08-30 23:30:39 +0530

    added docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node
+WORKDIR /app
+COPY package.json /app
+RUN npm install
+EXPOSE 3000
+COPY . /app/
+CMD ["node" , "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 const app = require('./app')
 const logger = require('./config/logger')
-app.listen(3000,()=>{
-    logger.info('server in running')
+const port = 3000
+app.listen(port,()=>{
+    logger.info(`server in running on ${port}`)
 })
